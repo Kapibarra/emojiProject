49aba8692067c52db1225138721206201b4b7115
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-09-02 14:02:33 +0300

    added burger btn

--- js/index.js
+++ js/index.js
@@ -16,24 +16,43 @@ for (let i = 0; i < acc.length; i++) {
 // lang select
 
 const langBtn = document.querySelector('.header__lang-item')
-const langWrapper = document.querySelector('.lang__list-wrapper')
+const overlayWrapper = document.querySelector('.lang__list-wrapper')
 langBtn.addEventListener('click', toggleLang)
-langWrapper.addEventListener('click', closeDropDown)
+overlayWrapper.addEventListener('click', closeLangDropDown)
 
 function toggleLang() {
-    const langWrapper = document.querySelector('.lang__list-wrapper')
+    const overlayWrapper = document.querySelector('.lang__list-wrapper')
     const langList = document.querySelector('.lang__list')
-    langWrapper.classList.add('visible')
-    langWrapper.classList.remove('unvisible')
+    overlayWrapper.classList.add('visible')
+    overlayWrapper.classList.remove('unvisible')
     langList.classList.add('visible')
     langList.classList.remove('unvisible') 
 }
 
-function closeDropDown() {
-    const langWrapper = document.querySelector('.lang__list-wrapper')
+function closeLangDropDown() {
+    const overlayWrapper = document.querySelector('.lang__list-wrapper')
     const langList = document.querySelector('.lang__list')
-    langWrapper.classList.add('unvisible')
-    langWrapper.classList.remove('visible')
+    const burgerNav = document.querySelector('.burger__nav')
+    overlayWrapper.classList.add('unvisible')
+    overlayWrapper.classList.remove('visible')
     langList.classList.add('unvisible')
     langList.classList.remove('visible') 
+    burgerNav.classList.remove('visible_nav')
+    burgerNav.classList.add('unvisible')
+
+}
+
+// Burger button 
+
+const burgerBtn = document.querySelector('.header__burger-nav')
+
+burgerBtn.addEventListener('click',toggleMenu)
+
+function toggleMenu() {
+    const overlayWrapper = document.querySelector('.lang__list-wrapper')
+    const burgerNav = document.querySelector('.burger__nav')
+    overlayWrapper.classList.add('visible')
+    overlayWrapper.classList.remove('unvisible')
+    burgerNav.classList.add('visible_nav')
+    burgerNav.classList.remove('unvisible')
 }
