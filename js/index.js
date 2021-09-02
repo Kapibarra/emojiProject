3096f5953414a852cd15b289d419e29b8a4ddd1c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-09-02 13:19:00 +0300

    added lang select

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 // accodrion
-
 const acc = document.getElementsByClassName("accordion");
-
-
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -18,8 +15,25 @@ for (let i = 0; i < acc.length; i++) {
 
 // lang select
 
-const langbtn = document.getElementsByClassName("header__lang-item")
+const langBtn = document.querySelector('.header__lang-item')
+const langWrapper = document.querySelector('.lang__list-wrapper')
+langBtn.addEventListener('click', toggleLang)
+langWrapper.addEventListener('click', closeDropDown)
 
-langbtn.addEventListener("click", () => {
-    const landList = document.getElementsByClassName("lang__list")
-})
+function toggleLang() {
+    const langWrapper = document.querySelector('.lang__list-wrapper')
+    const langList = document.querySelector('.lang__list')
+    langWrapper.classList.add('visible')
+    langWrapper.classList.remove('unvisible')
+    langList.classList.add('visible')
+    langList.classList.remove('unvisible') 
+}
+
+function closeDropDown() {
+    const langWrapper = document.querySelector('.lang__list-wrapper')
+    const langList = document.querySelector('.lang__list')
+    langWrapper.classList.add('unvisible')
+    langWrapper.classList.remove('visible')
+    langList.classList.add('unvisible')
+    langList.classList.remove('visible') 
+}
